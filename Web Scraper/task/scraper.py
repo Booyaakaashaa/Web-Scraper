96af2987d7067d8613975d46bfd33c9e3b66b8de
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-13 11:31:07 +0530

    Stage 1

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -6,3 +6,10 @@ req = requests.get(url)
 
 if req.status_code != 200:
     print("Invalid quote resource!")
+else:
+    try:
+        json_data = req.text
+        quote = json.loads(json_data)
+        print(quote["content"])
+    except KeyError:
+        print("Invalid quote resource!")
