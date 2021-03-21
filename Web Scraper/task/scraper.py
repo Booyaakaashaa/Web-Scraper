dc12eebc0906886d73948e72968c43ea4506b9c1
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-22 03:07:11 +0530

    Stage 2

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -6,19 +6,17 @@ import json
 output = {}
 url = input("Input the URL:\n")
 req = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-if req.status_code != 200 or "imdb" not in url:
+if req.status_code != 200 or "imdb" not in url or "name" in url:
     print("Invalid movie page!")
 else:
     soup = BeautifulSoup(req.content, 'html.parser')
     title = soup.find('meta', property='og:title')
     desc = soup.find('meta', property="og:description")
-    print(title['content'], desc['content'])
+    output["title"] = title["content"]
+    output["description"] = desc["content"]
+    #print(title['content'], desc['content'])
     """
     movie = json.loads(json_data.contents[0])
     output['title'] = movie["name"]
-    output["description"] = movie["trailer"]["description"]
+    output["description"] = movie["trailer"]["description"]"""
     print(output)
-
-
-
-"""
