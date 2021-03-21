8f4a3da7cb91118ce89dfbae7c374f4b806ca5f5
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-22 02:27:01 +0530

    Stage 1

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,14 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+
 import json
 
+output = {}
 url = input("Input the URL:\n")
-req = requests.get(url)
-
-if req.status_code != 200:
-    print("Invalid quote resource!")
+req = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+if req.status_code != 200 or "imdb" not in url:
+    print("Invalid movie page!")
 else:
-    json_data = req.text
-    quote = json.loads(json_data)
-    print(quote.get("content", "Invalid quote resource!"))
+    soup = BeautifulSoup(req.content, 'html.parser')
+    json_data = soup.find('script', type="application/ld+json")
+    quote = json.loads(json_data.contents[0])
+
+    #print(quote.get("content", "Invalid quote resource!"))
+    print(quote)
+
+
 
