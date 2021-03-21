1195f9e1fc82826e95850d5dd16a1c626bbb369b
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-21 22:07:02 +0530

    Stage 1

--- Problems/Get the paragraph/main.py
+++ Problems/Get the paragraph/main.py
@@ -1,3 +1,15 @@
 import requests
 
 from bs4 import BeautifulSoup
+
+word = input()
+link = input()
+
+response = requests.get(link)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+para = soup.find_all('p')
+for p in para:
+    if word in p.get_text():
+        print(p.get_text())
+        break
