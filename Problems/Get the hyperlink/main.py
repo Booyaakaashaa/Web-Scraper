7489159e6438c55699016677788a534c097eb8c9
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-21 16:41:12 +0530

    Stage 1

--- Problems/Get the hyperlink/main.py
+++ Problems/Get the hyperlink/main.py
@@ -1,3 +1,10 @@
 import requests
 
 from bs4 import BeautifulSoup
+
+num = int(input())
+url = input()
+pyg = requests.get(url)
+
+soup = BeautifulSoup(pyg.content, 'html.parser')
+print(soup.find_all('a', limit=num)[num - 1].get('href'))
