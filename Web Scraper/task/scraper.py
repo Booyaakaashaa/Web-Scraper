f52e061b10b625f01b03cf7968e57866be64bd3c
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-25 01:46:36 +0530

    Stage 4

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,10 +1,6 @@
 import requests
+import bs4 as Beautifulsoup
 
-url = input("Input the URL:\n")
-req = requests.get(url)
-if req.status_code != 200:
-    print(f"The URL returned {req.status_code}")
-else:
-    with open('source.html', 'wb') as scraped:
-        scraped.write(req.content)
-    print("\nContent saved.")
+url = "https://www.nature.com/nature/articles"
+response = requests.get(url)
+scrape = Beautifulsoup(response.content)
