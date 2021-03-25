c5a0202d9007a846659ba21774ae664ac5e276c0
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-26 00:01:15 +0530

    Stage 4

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 url = "https://www.nature.com/nature/articles"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
-article_data = soup.find_all("span")
+article_data = soup.find_all("News")
 
 
 
