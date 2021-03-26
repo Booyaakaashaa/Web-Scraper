c2fe503d27b57ff253edef8b692e66204495092f
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-27 04:00:21 +0530

    Stage 5

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -8,8 +8,8 @@ content_type = input()
 page = 1
 cwd = os.getcwd()
 while page <= page_num:
-    os.mkdir(f"Page_{page}")
-    os.chdir((os.path.join(cwd, f"Page_{page}")))
+    #os.mkdir(f"Page_{page}")
+    #os.chdir((os.path.join(cwd, f"Page_{page}")))
     url = f"https://www.nature.com/nature/articles?searchType=journalSearch&sort=PubDate&page={page}"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -22,9 +22,11 @@ while page <= page_num:
         file_name = new_soup.find("meta", {"name": "dc.title"})["content"]
         underscore = file_name.maketrans(" ", "_", string.punctuation)
         file_name = file_name.translate(underscore)
-        with open(f"{file_name}.txt", "wb") as output:
+        data = "".join(p.get_text() if p != "\n" else p for p in new_soup.find("div", {"class": "article-item__body"}).contents)
+        # print(data)
+        """with open(f"{file_name}.txt", "wb") as output:
             output.write(bytes(new_soup.get_text(), encoding="utf-8"))
-            print(file_name)
+            print(file_name)"""
     print(f"All files written and saved successfully from Page no. {page}")
     page += 1
     os.chdir(cwd)
